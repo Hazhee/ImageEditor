dbb3ee03b419143289d18d6e97f7e7375d4482f2
Elijah <elijah@Elijah-2.local>
2024-02-15 03:53:32 +0300

    f

--- app/RemoveBackgroundStrategy.php
+++ app/RemoveBackgroundStrategy.php
@@ -32,7 +32,7 @@ class RemoveBackgroundStrategy implements ImageEditingStrategy
 
         // create new manager instance with desired driver
         $name_gen = hexdec(uniqid()) . '.' . $request->file('image')->getClientOriginalExtension();
-        $manager = new ImageManager(new Driver);
+        $manager = ImageManager::gd();
 
         $img = $manager->read($request->file('image'));
         if ($resized_width > 0 && $resized_height > 0 || $rotation_angle != NULL) {
