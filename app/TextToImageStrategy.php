6552db2a48a0e343c7d6722de7e06f7358bf48e9
Elijah <elijah@Elijah-2.local>
2024-02-15 05:52:46 +0300

    laloi

--- app/TextToImageStrategy.php
+++ app/TextToImageStrategy.php
@@ -15,23 +15,25 @@ class TextToImageStrategy implements ImageEditingStrategy
     }
     public function process(Request $request)
     {
-        $request->validate([
-            'promt' => 'required|max:1000',
-        ]);
 
-        $response = Http::withHeaders([
-            'x-api-key' => $this->apiKey,
-        ])
-        ->attach('prompt', $request->promt)
-        ->post('https://clipdrop-api.co/text-to-image/v1');
+        dd($this->apiKey);
+        // $request->validate([
+        //     'promt' => 'required|max:1000',
+        // ]);
 
-        if ($response->successful()) {
-            $editedImagePath = hexdec(uniqid()) . '.png';
-            file_put_contents($editedImagePath, $response->body());
-            return view('edited_image')->with('editedImagePath', $editedImagePath);
-        } else {
-            // Handle the case when the API request is not successful
-            return back()->with(['error' => 'failed'], $response->status());
-        }
+        // $response = Http::withHeaders([
+        //     'x-api-key' => $this->apiKey,
+        // ])
+        // ->attach('prompt', $request->promt)
+        // ->post('https://clipdrop-api.co/text-to-image/v1');
+
+        // if ($response->successful()) {
+        //     $editedImagePath = hexdec(uniqid()) . '.png';
+        //     file_put_contents($editedImagePath, $response->body());
+        //     return view('edited_image')->with('editedImagePath', $editedImagePath);
+        // } else {
+        //     // Handle the case when the API request is not successful
+        //     return back()->with(['error' => 'failed'], $response->status());
+        // }
     }
 }
