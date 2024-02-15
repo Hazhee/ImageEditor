d13b0abbd649456aecb0fcde0bc8d831de71e4c4
Elijah <elijah@Elijah-2.local>
2024-02-16 02:59:53 +0300

    seec

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <!-- Bootstrap CSS link with integrity check -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script
 
     <!-- jQuery library script link -->
     <script class="jsbin" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
@@ -27,7 +28,7 @@
     @section('sidebar')
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-3">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#">Elijah's Image Editor</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
                     aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -36,30 +37,39 @@
                 <div class=" collapse navbar-collapse" id="navbarNavDropdown">
                     <ul class="navbar-nav ms-auto ">
                         <li class="nav-item">
-                            <a class="nav-link mx-2 active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link mx-2 active" aria-current="page" href="/">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link mx-2" href="#">Products</a>
+                            <a class="nav-link mx-2" href="{{ url('https://elijahimageeditor-bf08cb0934b4.herokuapp.com/remove/background') }}">Background Remove</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link mx-2" href="#">Pricing</a>
+                            <a class="nav-link mx-2" href="#">Image Clean Up </a>
                         </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link mx-2 dropdown-toggle" href="#" id="navbarDropdownMenuLink"
-                                role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Company
+
+                        <li class="nav-item">
+                            <a class="nav-link mx-2" href="#">Image upscaling
                             </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a class="dropdown-item" href="#">Blog</a></li>
-                                <li><a class="dropdown-item" href="#">About Us</a></li>
-                                <li><a class="dropdown-item" href="#">Contact us</a></li>
-                            </ul>
                         </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link mx-2" href="#">Text To Image
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link mx-2" href="#">Crop Image </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link mx-2" href="#">Replace background
+                            </a>
+                        </li>
+
                     </ul>
                 </div>
             </div>
         </nav>
-    @endsection
+    @show
     <!-- Container div for the content -->
     <div class="container mt-5">
         <!-- Placeholder for the content, to be filled by the specific view -->
