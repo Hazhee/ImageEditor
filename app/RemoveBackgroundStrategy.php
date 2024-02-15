ff7ad887bb41a075c4d0b9e2fdcd6902195ce954
Elijah <elijah@Elijah-2.local>
2024-02-15 19:19:37 +0300

    seii

--- app/RemoveBackgroundStrategy.php
+++ app/RemoveBackgroundStrategy.php
@@ -4,7 +4,7 @@ namespace App;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
-use Intervention\Image\Drivers\Imagick\Driver;
+use Intervention\Image\Drivers\Gd\Driver;
 use Intervention\Image\ImageManager;
 
 class RemoveBackgroundStrategy implements ImageEditingStrategy
