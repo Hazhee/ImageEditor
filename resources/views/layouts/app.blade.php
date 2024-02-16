7e80b16e8c9c12cba11e9db72d9640922185d889
Elijah <elijah@Elijah-2.local>
2024-02-16 03:07:45 +0300

    see

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,27 +8,20 @@
     <title>Elijah Image Editor</title>
 
     <!-- Bootstrap CSS link with integrity check -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script
-
-    <!-- jQuery library script link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script class="jsbin" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-
     <!-- Cropper.js CSS link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.min.css">
-
     <!-- Custom stylesheet link -->
     <link rel="stylesheet" href="https://elijahimageeditor-bf08cb0934b4.herokuapp.com/assets/css/style.css">
-    {{-- <link rel="stylesheet" href="{{asset('assets/css/style.js')}}"> --}}
-
 </head>
 
 <body>
     @section('sidebar')
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-3">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Elijah's Image Editor</a>
+                <a class="navbar-brand" href="/">Elijah's Image Editor</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
                     aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -40,28 +33,28 @@
                             <a class="nav-link mx-2 active" aria-current="page" href="/">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link mx-2" href="{{ url('https://elijahimageeditor-bf08cb0934b4.herokuapp.com/remove/background') }}">Background Remove</a>
+                            <a class="nav-link mx-2" href="{{ route('background.remove.index') }}">Background Remove</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link mx-2" href="#">Image Clean Up </a>
+                            <a class="nav-link mx-2" href="{{ route('clean') }}">Image Clean Up </a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link mx-2" href="#">Image upscaling
+                            <a class="nav-link mx-2" href="{{ route('image.upscaling.index') }}">Image upscaling
                             </a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link mx-2" href="#">Text To Image
+                            <a class="nav-link mx-2" href="{{ route('text.image.index') }}">Text To Image
                             </a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link mx-2" href="#">Crop Image </a>
+                            <a class="nav-link mx-2" href="{{ route('image.crop.index') }}">Crop Image </a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link mx-2" href="#">Replace background
+                            <a class="nav-link mx-2" href="{{ route('replace.background.index') }}">Replace background
                             </a>
                         </li>
 
